1496c9a39e26402afed4c8e6e1ad0bd470576696
euberdeveloper <euberdeveloper@gmail.com>
2023-05-05 23:43:09 +0200

    refactor: rename inventary -> inventory
    BREAKING CHANGE: all the refs to inventary are changed to inventory

--- source/utils/writeMarkdownIndex.ts
+++ source/utils/writeMarkdownIndex.ts
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
-export function writeMarkdownIndex(inventaryPath: string, content: string): void {
-    const indexPath = path.join(inventaryPath, 'README.md');
+export function writeMarkdownIndex(inventoryPath: string, content: string): void {
+    const indexPath = path.join(inventoryPath, 'README.md');
     fs.writeFileSync(indexPath, content);
 }
